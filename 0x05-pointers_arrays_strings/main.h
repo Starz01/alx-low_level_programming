d0a3e99951dac93b1051c929f362197d988658b7
Fred Kasirye <kasiryefred@gmail.com>
2023-02-28 13:11:43 -0800

    a

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef main_h
+#define main_h
+
 int _putchar;
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -8,3 +11,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+#endif
